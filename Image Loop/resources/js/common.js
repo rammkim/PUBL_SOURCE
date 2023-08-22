27862e51d55d629fcfc13647d781dd2fa0af7f5d
DESKTOP-G61730P\MSI <rlaqhfka8152@hanmail.net>
2023-08-22 23:40:51 +0900

    [Image Loop]

---
+++ Image Loop/resources/js/common.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false
+    },
+    allowTouchMove: false,
+    speed: 15000,
+    loop: true,
+    loopAdditionalSlides: 1
+});
